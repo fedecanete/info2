94fb9afe3ce860ce74c742b3b6859427c3147c8b
LucasUsh <lucaas-e@hotmail.com>
2017-11-06 17:43:27 -0300

    Creo nueva carpeta para trabajar con LPCXpresso allí

--- MenuLCD/0-TPORiego/Debug/src/FW_Drivers/cr_startup_lpc175x_6x.d
+++
@@ -1,3 +0,0 @@
-src/FW_Drivers/cr_startup_lpc175x_6x.o \
- src/FW_Drivers/cr_startup_lpc175x_6x.d: \
- ../src/FW_Drivers/cr_startup_lpc175x_6x.c
--- MenuLCD/0-TPORiego/Debug/src/FW_Drivers/crp.d
+++
@@ -1,1 +0,0 @@
-src/FW_Drivers/crp.o src/FW_Drivers/crp.d: ../src/FW_Drivers/crp.c
