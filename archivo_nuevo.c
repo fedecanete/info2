f2722e595cce45d5fe50828b9082d53646658e86
LucasUsh <lucaas-e@hotmail.com>
2017-11-10 17:39:13 -0300

    clean

--- archivo_nuevo.c
+++
@@ -1,6 +0,0 @@
-//Este archivo es creado desde la web, LUEGO de la petición de fusión a fedecanete
-
-#include <stdio.h>        //(?)
-
-//Luego de crear el archivo, la petición de fusión sigue siendo sólo para el primer commit.
-//O sea, la modificación de prueba.txt
